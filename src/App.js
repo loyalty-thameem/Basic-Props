f7b7189583d3e9245aa1789bd52fd8f0591bd1aa
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-10 15:56:25 +0530

    PropTypes and defaultProps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ParentComponent from './components/ParentComponent';
+import ChildComponent from './components/ChildComponent';
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <ParentComponent />
+        {/* <ParentComponent /> */}
+        <ChildComponent />
       </div>
     );
   }
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 export default class ChildComponent extends React.Component {
   render() {
     return (
       <div>
-        <h1>============ Methods as Props ==============</h1>
-        <button type="button" onClick={() => this.props.handleClick()}>
-          Greet Parent
-        </button>
+        <h1>
+          {this.props.arrayProp}
+          <br />
+          {this.props.stringProp}
+          <br />
+          {this.props.numberProp}
+          <br />
+          {this.props.booleanProp}
+        </h1>
       </div>
     );
   }
 }
+// Validating prop types
+ChildComponent.propTypes = {
+  arrayProp: PropTypes.array,
+  stringProp: PropTypes.string,
+  numberProp: PropTypes.number,
+  booleanProp: PropTypes.boolean,
+};
+//Creating default prop
+ChildComponent.defaultProps = {
+  arrayProp: ['thameem', 'ansari', 'pop'],
+  stringProp: 'myWorld',
+  numberProp: 10,
+  booleanProp: true,
+};
