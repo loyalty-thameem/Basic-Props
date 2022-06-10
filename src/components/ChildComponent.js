c3121ff4f0ecb971044ce65c149d1e21eeae148c
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-10 17:35:29 +0530

    statice defaultProps

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 export default class ChildComponent extends React.Component {
+  // Component inside default props use
+  static defaultProps = {
+    name: 'stranger',
+  };
   render() {
     return (
       <div>
-        <h1>
+        <h4>Default Props: {this.props.name}</h4>
+        {/* <h1>
           {this.props.arrayProp.map((item, index) => {
             return `Student ${index + 1} :${item + (index != 2 ? ', ' : '\n')}`;
           })}
@@ -14,22 +19,22 @@ export default class ChildComponent extends React.Component {
           {this.props.numberProp}
           <br />
           {this.props.booleanProp}
-        </h1>
+        </h1> */}
       </div>
     );
   }
 }
 // Validating prop types
-ChildComponent.propTypes = {
-  arrayProp: PropTypes.array,
-  stringProp: PropTypes.string,
-  numberProp: PropTypes.number,
-  booleanProp: PropTypes.boolean,
-};
+// ChildComponent.propTypes = {
+//   arrayProp: PropTypes.array,
+//   stringProp: PropTypes.string,
+//   numberProp: PropTypes.number,
+//   booleanProp: PropTypes.boolean,
+// };
 //Creating default prop
-ChildComponent.defaultProps = {
-  arrayProp: ['thameem', 'ansari', 'pop'],
-  stringProp: 'myWorld',
-  numberProp: 10,
-  booleanProp: true,
-};
+// ChildComponent.defaultProps = {
+//   arrayProp: ['thameem', 'ansari', 'pop'],
+//   stringProp: 'myWorld',
+//   numberProp: 10,
+//   booleanProp: true,
+// };
